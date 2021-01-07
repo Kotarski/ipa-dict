bc8fff92786e92b379c2efd15efb40b4b96c8202
Kotarski <edktrsk@gmail.com>
2021-01-07 16:32:00 +0000

    check size too

--- test/import.cjs
+++ test/import.cjs
@@ -3,6 +3,7 @@ const japaneseIpaMap = require("ipa-dict/ja.js");
 
 describe('commonjs import', function () {
     it('Correctly imported a map instance', function () {
-        assert(japaneseIpaMap instanceof Map)
+        assert(japaneseIpaMap instanceof Map);
+        assert(japaneseIpaMap.size > 0);
     });
 });
--- test/import.mjs
+++ test/import.mjs
@@ -3,6 +3,7 @@ import japaneseIpaMap from "ipa-dict/ja.js";
 
 describe('module import', function () {
     it('Correctly imported a map instance', function () {
-        assert(japaneseIpaMap instanceof Map)
+        assert(japaneseIpaMap instanceof Map);
+        assert(japaneseIpaMap.size > 0);
     });
 });
